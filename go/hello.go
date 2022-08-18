3b60d4fd242d0b1c40259157799b1fcc37f8f685
Ross Clark <rosscee1998@gmail.com>
2022-08-18 12:14:40 +0100

    break validator fn logic into smaller helpers

--- go/hello.go
+++ go/hello.go
@@ -5,65 +5,69 @@ import (
 )
 
 func main() {
-	solution1 := [][]int{
-		{8, 2, 9, 3, 6, 5, 1, 4, 7},
-		{6, 4, 3, 7, 1, 8, 5, 1, 9},
-		{7, 5, 1, 4, 9, 2, 6, 3, 8},
-		{3, 1, 8, 5, 2, 7, 4, 9, 6},
-		{5, 9, 6, 1, 4, 1, 7, 8, 2},
-		{4, 7, 2, 9, 8, 6, 3, 1, 5},
-		{9, 8, 4, 6, 5, 1, 2, 7, 3},
-		{2, 6, 7, 8, 3, 4, 9, 5, 1},
-		{9, 3, 5, 2, 7, 9, 8, 6, 4},
-	}
-	solution2 := [][]int{
-		{5, 8, 6, 4, 3, 7, 1, 9, 2},
-		{1, 9, 4, 5, 8, 2, 3, 6, 7},
-		{7, 2, 3, 9, 6, 1, 4, 5, 8},
-		{2, 4, 7, 1, 9, 8, 6, 3, 5},
-		{8, 3, 9, 6, 2, 4, 7, 2, 1},
-		{6, 5, 1, 7, 2, 3, 8, 4, 9},
-		{9, 7, 5, 3, 1, 6, 7, 8, 4},
-		{3, 1, 8, 2, 4, 5, 9, 7, 6},
-		{4, 6, 2, 8, 7, 9, 5, 1, 3},
-	}
-	solution3 := [][]int{
-		{1, 8, 3, 2, 7, 4, 6, 5, 9},
-		{9, 7, 4, 5, 8, 6, 3, 2, 1},
-		{2, 6, 5, 1, 9, 3, 7, 4, 8},
-		{5, 9, 2, 8, 3, 1, 4, 6, 7},
-		{8, 4, 6, 7, 2, 5, 9, 1, 3},
-		{7, 3, 1, 4, 6, 9, 2, 8, 5},
-		{3, 5, 9, 6, 4, 8, 1, 7, 2},
-		{6, 1, 7, 3, 5, 2, 8, 9, 4},
-		{4, 2, 8, 9, 1, 7, 5, 3, 6},
+	solutions := [][][]int{
+		{
+			{8, 2, 9, 3, 6, 5, 1, 4, 7},
+			{6, 4, 3, 7, 1, 8, 5, 1, 9},
+			{7, 5, 1, 4, 9, 2, 6, 3, 8},
+			{3, 1, 8, 5, 2, 7, 4, 9, 6},
+			{5, 9, 6, 1, 4, 1, 7, 8, 2},
+			{4, 7, 2, 9, 8, 6, 3, 1, 5},
+			{9, 8, 4, 6, 5, 1, 2, 7, 3},
+			{2, 6, 7, 8, 3, 4, 9, 5, 1},
+			{9, 3, 5, 2, 7, 9, 8, 6, 4},
+		},
+		{
+			{5, 8, 6, 4, 3, 7, 1, 9, 2},
+			{1, 9, 4, 5, 8, 2, 3, 6, 7},
+			{7, 2, 3, 9, 6, 1, 4, 5, 8},
+			{2, 4, 7, 1, 9, 8, 6, 3, 5},
+			{8, 3, 9, 6, 2, 4, 7, 2, 1},
+			{6, 5, 1, 7, 2, 3, 8, 4, 9},
+			{9, 7, 5, 3, 1, 6, 7, 8, 4},
+			{3, 1, 8, 2, 4, 5, 9, 7, 6},
+			{4, 6, 2, 8, 7, 9, 5, 1, 3},
+		},
+		{
+			{1, 8, 3, 2, 7, 4, 6, 5, 9},
+			{9, 7, 4, 5, 8, 6, 3, 2, 1},
+			{2, 6, 5, 1, 9, 3, 7, 4, 8},
+			{5, 9, 2, 8, 3, 1, 4, 6, 7},
+			{8, 4, 6, 7, 2, 5, 9, 1, 3},
+			{7, 3, 1, 4, 6, 9, 2, 8, 5},
+			{3, 5, 9, 6, 4, 8, 1, 7, 2},
+			{6, 1, 7, 3, 5, 2, 8, 9, 4},
+			{4, 2, 8, 9, 1, 7, 5, 3, 6},
+		},
 	}
 
-	fmt.Println(ValidateSolution(solution1))
-	fmt.Println(ValidateSolution(solution2))
-	fmt.Println(ValidateSolution(solution3))
+	for _, sol := range solutions {
+		fmt.Println(validSolution(sol))
+	}
 }
 
-func ValidateSolution(solution [][]int) (result bool) {
+func validSolution(solution [][]int) (result bool) {
 	for i, row := range solution {
-		// check row
-		isValidRow := ValidateArray(row)
+		isValidRow := validSequence(row)
 		if !isValidRow {
 			return false
 		}
 
-		// check col
-		var col []int
-		for j := range solution {
-			col = append(col, solution[j][i])
-		}
-		isValidCol := ValidateArray(col)
+		isValidCol := validCol(i, solution)
 		if !isValidCol {
 			return false
 		}
 	}
 
-	// check grids
+	hasValidGrids := validGrids(solution)
+	if !hasValidGrids {
+		return false
+	}
+
+	return true
+}
+
+func validGrids(solution [][]int) (valid bool) {
 	for i := 0; i < 9; i += 3 {
 		rowSubset := solution[i : i+3]
 		for j := 0; j < 9; j += 3 {
@@ -71,21 +75,28 @@ func ValidateSolution(solution [][]int) (result bool) {
 			for _, row := range rowSubset {
 				grid = append(grid, row[j], row[j+1], row[j+2])
 			}
-			isValidGrid := ValidateArray(grid)
+			isValidGrid := validSequence(grid)
 			if !isValidGrid {
 				return false
 			}
 		}
 	}
-
 	return true
 }
 
-func ValidateArray(array []int) (result bool) {
-	return (Min(array) == 1) && (Max(array) == 9) && !ContainsDuplicate(array)
+func validCol(i int, solution [][]int) (valid bool) {
+	var col []int
+	for j := range solution {
+		col = append(col, solution[j][i])
+	}
+	return validSequence(col)
 }
 
-func Min(array []int) (min int) {
+func validSequence(array []int) (result bool) {
+	return (min(array) == 1) && (max(array) == 9) && !hasDuplicate(array)
+}
+
+func min(array []int) (min int) {
 	for index, element := range array {
 		if index == 0 || element < min {
 			min = element
@@ -94,7 +105,7 @@ func Min(array []int) (min int) {
 	return min
 }
 
-func Max(array []int) (max int) {
+func max(array []int) (max int) {
 	for index, element := range array {
 		if index == 0 || element > max {
 			max = element
@@ -103,7 +114,7 @@ func Max(array []int) (max int) {
 	return max
 }
 
-func ContainsDuplicate(array []int) (result bool) {
+func hasDuplicate(array []int) (result bool) {
 	entries := make(map[int]bool, 0)
 	for _, element := range array {
 		if entries[element] {
